26b703fad314472eca2f76effb813d63ca9b0ca7
francois <francoisrvespa@gamil.com>
2014-05-24 14:05:44 -0500

    added list and command list-install

--- bin/dude.js
+++ bin/dude.js
@@ -5,18 +5,54 @@ var $ = require;
 var domain = $('domain').create();
 
 domain.on('error', function (error) {
-  console.log('error', error.stack);
+  console.log('error'.red, error.stack.yellow);
 });
 
 domain.run(function () {
+  $('colors');
+
   var program = $('commander');
 
   var package = $('../package.json');
 
   program
     .version(package.version)
-    .option('install', 'Install new dependency')
-    .parse(process.argv);
+    .option('list-all', 'list available dependencies for download')
+    .option('list-installed', 'list installed dependencies')
+    .option('search [<dependency>]', 'search for a dependency based on its name')
+    .option('install <dependency>[@<version>]', 'install new dependency');
 
+  program.on('--help', function () {
+    console.log('  Examples:');
+    console.log('');
+    console.log('    $ dude-js search mongodb');
+    console.log('    $ dude-js install mongodb');
+    console.log('    $ dude-js start mongodb');
+    console.log('    $ dude-js status mongodb');
+    console.log('    $ dude-js stop mongodb');
+    console.log('    $ dude-js uninstall mongodb');
+    console.log('');
+  });
 
+  program.on('install', function (dependency) {
+    $('../lib/install/' + dependency)(domain.intercept(function () {
+      
+    }));
+  });
+
+  program.on('list-all', function () {
+    var list = $('../list.json');
+
+    console.log();
+    console.log(('  dude.js | ' + list.length + ' available dependencies for download').bold.blue);
+    console.log();
+
+    list.forEach(function (dependency) {
+      console.log('  ' + dependency.name + ' ' + dependency.slug.cyan + '', '      ', dependency.about);
+    });
+
+    console.log();
+  });
+
+  program.parse(process.argv);
 });
