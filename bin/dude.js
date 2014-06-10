caf22c6335e6bf5653253e0335c4bdf2fc4dd5e7
francois <francoisrvespa@gamil.com>
2014-06-10 10:18:51 -0500

    fixed #24

--- bin/dude.js
+++ bin/dude.js
@@ -11,238 +11,232 @@ domain.on('error', function (error) {
 domain.run(function () {
   $('colors');
 
-  var package = $('../package.json');
+  var program = $('../lib/program');
 
-  var command = {
-    actions: {
-      "init": {
-        about: 'install necessary folder for dude-js in current directory',
-        action: function () {
-          $('../lib/init')(domain.intercept(function () {
-            console.log('dude.js init ok'.green);
-          }));
+  program
+
+    .package($('../package.json'))
+    
+    .action('init')
+      .about('install necessary folder for dude-js in current directory')
+      .run(function () {
+        $('../lib/init')(domain.intercept(function () {
+          console.log('dude.js init ok'.green);
+        }));
+      })
+
+    .action('available')
+      .about('List all available dude dependencies')
+      .run(function () {
+        var list = $('../list.json');
+
+        console.log();
+        console.log(('  dude.js | ' + list.length + ' available dependencies for download').bold.blue);
+        console.log();
+
+        list.forEach(function (dependency) {
+          console.log('  ' + dependency.name + ' ' + dependency.slug.cyan + '', '      ', dependency.about);
+        });
+
+        console.log();
+      })
+
+    .action('installed')
+      .about('List installed dude dependencies')
+      .run(function () {
+        var list;
+
+        try {
+          list = $($('path').join(process.env.PWD, 'dude.json'));
         }
-      },
+        catch ( error ) {
+          return console.log('  Nothing installed yet!'.yellow);
+        }
 
+        if ( ! list.dependencies ) {
+          return console.log('  Nothing installed yet!'.yellow);
+        }
 
-      "available": {
-        about: 'List all available dude dependencies',
-        action: function () {
+        var depLength = Object.keys(list.dependencies).length;
+
+        console.log();
+        console.log(('  dude.js | ' + depLength + ' dependenc' +
+          ( depLength > 1 ? 'ies' : 'y' ) + ' installed').bold.blue);
+        console.log();
+
+        for ( var dep in list.dependencies ) {
+          console.log('  ' + dep + ' version ' + list.dependencies[dep]);
+        }
+
+        console.log();
+      })
+
+    .action('install')
+      .about('Install a new dependency')
+      .run(function (dependency) {
+        var list = $('../list.json');
+
+        var Dependency;
+
+        list.forEach(function (dep) {
+          if ( dep.slug === dependency ) {
+            Dependency = dep;
+          }
+        });
+
+        if ( ! Dependency ) {
+          return console.log('No such dependency'.red, ('Dependency not found: ' + dependency).yellow);
+        }
+
+        var version = Dependency.latest;
+
+        $('../lib/install')(dependency, version, domain.intercept(function (version, dir) {
+          console.log(('  Successfully installed ' + dependency.bold + ' version ' + version.bold).green);
+        }));
+      })
+
+    .action('start')
+      .about('start a service or a script')
+      .run(function (service) {
+        var options = {};
+
+        if ( /\.js$/.test(service) ) {
+
+        }
+        
+        else {
           var list = $('../list.json');
 
-          console.log();
-          console.log(('  dude.js | ' + list.length + ' available dependencies for download').bold.blue);
-          console.log();
-
-          list.forEach(function (dependency) {
-            console.log('  ' + dependency.name + ' ' + dependency.slug.cyan + '', '      ', dependency.about);
-          });
-
-          console.log();
-        }
-      },
-
-
-      "installed":  {
-        about: 'List installed dude dependencies',
-        action: function () {
-          var list;
-
-          try {
-            list = $($('path').join(process.env.PWD, 'dude.json'));
-          }
-          catch ( error ) {
-            return console.log('  Nothing installed yet!'.yellow);
-          }
-
-          if ( ! list.dependencies ) {
-            return console.log('  Nothing installed yet!'.yellow);
-          }
-
-          var depLength = Object.keys(list.dependencies).length;
-
-          console.log();
-          console.log(('  dude.js | ' + depLength + ' dependenc' +
-            ( depLength > 1 ? 'ies' : 'y' ) + ' installed').bold.blue);
-          console.log();
-
-          for ( var dep in list.dependencies ) {
-            console.log('  ' + dep + ' version ' + list.dependencies[dep]);
-          }
-
-          console.log();
-        }
-      },
-
-
-      "install": {
-        about: 'Install a new dependency',
-        action: function (dependency) {
-          var list = $('../list.json');
-
-          var Dependency;
+          var Service;
 
           list.forEach(function (dep) {
-            if ( dep.slug === dependency ) {
-              Dependency = dep;
+            if ( dep.slug === service ) {
+              Service = dep;
             }
           });
 
-          if ( ! Dependency ) {
-            return console.log('No such dependency'.red, ('Dependency not found: ' + dependency).yellow);
+          if ( ! Service ) {
+            throw new Error('No such service: ' + service);
           }
 
-          var version = Dependency.latest;
+          options = Service.start['arguments-style'] || [];
 
-          $('../lib/install')(dependency, version, domain.intercept(function (version, dir) {
-            console.log(('  Successfully installed ' + dependency.bold + ' version ' + version.bold).green);
-          }));
-        }
-      },
-
-
-      "start": {
-        about: 'start a service or a script',
-        action: function (service) {
-          var options = {};
-
-          if ( /\.js$/.test(service) ) {
-
-          }
-          
-          else {
-            var list = $('../list.json');
-
-            var Service;
-
-            list.forEach(function (dep) {
-              if ( dep.slug === service ) {
-                Service = dep;
-              }
-            });
-
-            if ( ! Service ) {
-              throw new Error('No such service: ' + service);
+          for ( var i = 4, bits; i < process.argv.length; i ++ ) {
+            if ( $('util').isArray(options) ) {
+              options.push(process.argv[i]);
             }
-
-            options = Service.start['arguments-style'] || [];
-
-            for ( var i = 4, bits; i < process.argv.length; i ++ ) {
-              if ( $('util').isArray(options) ) {
-                options.push(process.argv[i]);
-              }
-              else {
-                bits = process.argv[i].split('=');
-                options[bits[0]] = bits[1];
-              }
+            else {
+              bits = process.argv[i].split('=');
+              options[bits[0]] = bits[1];
             }
           }
-
-          $('../lib/start')(service, options || '{}', domain.intercept(function (status) {
-            console.log('started'.green, status);
-          }));
         }
-      },
 
+        $('../lib/start')(service, options || '{}', domain.intercept(function (status) {
+          console.log('started'.green, status);
+        }));
+      })
 
-      "running": {
-        about: 'Show running services and scripts',
-        action: function () {
-          $('../lib/running')(domain.intercept(function (running) {
-            if ( ! running.length ) {
-              return console.log('    Nothing running'.yellow);
-            }
-            running.forEach(function (item) {
-              console.log(item.service.green.bold + '@'.grey + item.version.cyan,
-                'pid:'.grey, item.pid.toString().cyan,
-                'arguments:'.grey, JSON.stringify(item.arguments).cyan);
-
-              console.log('    ' + 'log:'.grey, item.log.magenta,
-                'started:'.grey, $('moment')(item.started).fromNow().magenta);
-
-              if ( item.zombie ) {
-                console.log('    ' + 'ZOMBIE ALERT'.red.bold, 'It seems this service died unexpectedly'.yellow);
-              }
-            });
-          }));
-        }
-      },
-
-
-      "stop": {
-        about: 'Stop a service or a script',
-        action: function (service) {
-          if ( ! service ) {
-            service = true;
+    .action('running')
+      .about('Show running services and scripts')
+      .run(function () {
+        $('../lib/running')(domain.intercept(function (running) {
+          if ( ! running.length ) {
+            return console.log('    Nothing running'.yellow);
           }
-          $('../lib/stop')(service, domain.intercept(function (stopped) {
-            if ( service === true && ! stopped.length ) {
-              return console.log('Nothing is running so nothing was stooped'.yellow);
+          running.forEach(function (item) {
+            console.log(item.service.green.bold + '@'.grey + item.version.cyan,
+              'pid:'.grey, item.pid.toString().cyan,
+              'arguments:'.grey, JSON.stringify(item.arguments).cyan);
+
+            console.log('    ' + 'log:'.grey, item.log.magenta,
+              'started:'.grey, $('moment')(item.started).fromNow().magenta);
+
+            if ( item.zombie ) {
+              console.log('    ' + 'ZOMBIE ALERT'.red.bold, 'It seems this service died unexpectedly'.yellow);
             }
-            stopped.forEach(function (s) {
-              console.log(((s.service || s.script) + ' has been stopped').green, ('pid ' + s.pid + ' ID ' + s.log).grey);
-            });
-          }));
+          });
+        }));
+      })
+
+    .action('stop')
+      .about('Stop a service or a script')
+      .run(function (service) {
+        if ( ! service ) {
+          service = true;
         }
-      },
+        $('../lib/stop')(service, domain.intercept(function (stopped) {
+          if ( service === true && ! stopped.length ) {
+            return console.log('Nothing is running so nothing was stooped'.yellow);
+          }
+          stopped.forEach(function (s) {
+            console.log(((s.service || s.script) + ' has been stopped').green, ('pid ' + s.pid + ' ID ' + s.log).grey);
+          });
+        }));
+      })
 
+    .action('reload')
+      .about('Reload a script')
+      .run(function (script) {
+        $('../lib/reload')(script, domain.intercept(function (message) {
+          console.log((script + ' has been reloaded').green);
+        }));
+      })
 
-      "reload": {
-        about: 'Reload a script',
-        action: function (script) {
-          $('../lib/reload')(script, domain.intercept(function (message) {
-            console.log((script + ' has been reloaded').green);
-          }));
+    .action('bin')
+      .about('Shortcut access to the bin folder of a dependency')
+      .run(function (dependency, script) {
+        var args = process.argv.filter(function (arg, argi) {
+          if ( argi > 4 ) {
+            return true;
+          }
+        });
+        $('../lib/bin')(dependency, script, args || [], domain.intercept(function (bin) {
+          console.log(bin);
+        }));
+      })
+
+    .action('config')
+      .about('Set/Get dude.js configuration')
+      .usage('Set a new build rule', [
+          {
+            name: 'set',
+            required: true,
+            type: 'keyword'
+          },
+          {
+            name: 'build',
+            required: true,
+            type: 'keyword'
+          },
+          {
+            name: 'build-technology',
+            required: true,
+            type: 'value'
+          },
+          {
+            name: 'files',
+            required: true,
+            type: 'keyword'
+          },
+          {
+            name: 'source-file',
+            required: true,
+            type: 'value'
+          },
+          {
+            name: 'destination-file',
+            required: true,
+            type: 'value'
+          }
+        ])
+      .run(function (verb, action) {
+        if ( typeof verb !== 'string' ) {
+          throw new Error('Verb must be an action');
         }
-      },
 
-      "bin": {
-        about: 'Shortcut access to the bin folder of a dependency',
-        action: function (dependency, script) {
-          $('../lib/bin')(dependency, script, domain.intercept(function (bin) {
-            console.log(bin);
-          }));
-        }
-      }
+        var config = $('../lib/config');
+      })
 
-
-    }
-  };
-
-  if ( ! process.argv[2] || ['-h', '--help', 'help'].indexOf(process.argv[2]) > -1 ) {
-    console.log();
-    console.log('    ' + [package.name, 'v' + package.version, '|', package.description].join(' ').green);
-
-    console.log();
-    console.log('    Usage:');
-    console.log();
-    console.log('        dudejs <action> [<arguments>...]'.magenta);
-    console.log();
-
-
-    console.log();
-    console.log('    Actions:');
-    console.log();
-
-    for ( var action in command.actions ) {
-      for ( var space1 = '', i = action.length; i < 15 ; i ++, space1 += ' ' );
-
-      console.log('    ' + action.bold.cyan + space1 + command.actions[action].about);
-    }
-
-    console.log();
-    console.log('    Full documentation at https://github.com/co2-git/dude'.grey);
-    console.log();
-
-    return;
-  }
-
-  if ( command.actions[process.argv[2]] ) {
-    command.actions[process.argv[2]].action.apply(null, process.argv.reduce(function (args, arg, argi) {
-      if ( argi > 2 ) {
-        args.push(arg);
-      }
-      return args;
-    }, []));
-  }
+    .exec();
 });
